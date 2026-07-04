50b42073ace17555e3eece1a41fe4a4e5585d8ed
AchieverBangs <angelachiever361@gmail.com>
2026-07-04 23:18:55 +0000

    Allow GitHub Pages origin in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3000',
     'null', // file:// requests have Origin: null
+    'https://achieverbangs.github.io',
 ];
 
 app.use(cors({
